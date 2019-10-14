e6396d8a781b4360443c71b5caed190eb0aeeea9
Jensen <loganvj@byui.edu>
2019-10-14 17:10:22 -0600

    hello

--- R/hello.R
+++ R/hello.R
@@ -7,11 +7,6 @@
 #
 #   http://r-pkgs.had.co.nz/
 #
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 
 library(tidyverse)
 
